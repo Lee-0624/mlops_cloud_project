fa4c4767079325616e64eae575284e3f072c39ce
jihun.ryu <mahomi3@gmail.com>
2025-05-28 00:42:54 +0900

    fix: KMA API URL 및 인증 키 파라미터 수정

--- src/data_ingest.py
+++ src/data_ingest.py
@@ -1,19 +1,27 @@
 import requests, os, pandas as pd, datetime as dt, sys
 from dateutil.relativedelta import relativedelta
 
-KMA_KEY = os.environ["KMA_API_KEY"]
-BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+KMA_API_KEY = os.environ["KMA_API_KEY"]
+
+# BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+# BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
+BASE_URL =  "https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstInfoService_2.0/getVilageFcst"
+
 
 def run():
     base_date = (dt.datetime.utcnow() + relativedelta(hours=9)).strftime("%Y%m%d")
+    api_key_name = "authKey" if BASE_URL.startswith("https://apihub.kma.go.kr") else "serviceKey"
     params = {
-        "serviceKey": KMA_KEY,
+        api_key_name: KMA_API_KEY,
         "pageNo": "1", "numOfRows": "1000",
         "dataType": "JSON",
         "base_date": base_date, "base_time": "0200",
         "nx": "60", "ny": "127"
     }
+    print(f"BASE_URL: {BASE_URL}, params: {params}")
     r = requests.get(BASE_URL, params=params, timeout=10)
+    print(f"r: {r}")
+    print(f"r.text: {r.text}")
     r.raise_for_status()
     items = r.json()["response"]["body"]["items"]["item"]
     df = pd.DataFrame(items)
